ab2903eed4e3890d82f0287ed0ceade39cc2c670
Alex Fu <alex.yfu@gmail.com>
2024-01-29 23:38:28 +0800

    builder-02 solution

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,42 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
 
-// Key 1. #[proc_macro_derive(???)], set right derive name: Builder
-// Key 2. Import TokenStream
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // Key 2. Key parse function and DeriveInput struct.
+    let ast = parse_macro_input!(input as DeriveInput);
+    // Pretty print DeriveInput if syn extra-traits enabled.
+    // println!("{:#?}", ast);
 
-    // Key 3. Return TokenStream by calling quote!{}.into()
-    let expanded = quote! {};
+    // Key 3. Ident is not "String" and how to construct an ident.
+    let derive_struct_ident = ast.ident;
+    // It won't compile if this is String type, must be ident for struct name.
+    let builder_struct_ident = format_ident!("{}Builder", derive_struct_ident);
+
+    // Key 4.1 Get to know quote! macro. struct CommandBuilder.
+    let builder_definition_block = quote! {
+        // no fields needed for test 2
+        pub struct #builder_struct_ident {}
+    };
+
+    // Key 5. Get to know quote! macro. Impl builder() function.
+    let builder_constructor_block = quote! {
+        impl #derive_struct_ident {
+            pub fn builder() -> #builder_struct_ident {
+                #builder_struct_ident {
+                    // no fields need for test 2.
+                }
+            }
+        }
+    };
+
+    // Key 1. Macro generated code is here. What we need to do is adding new code blocks.
+    // Code blocks can be populated by other code blocks.
+    let expanded = quote! {
+        #builder_definition_block
+
+        #builder_constructor_block
+    };
     expanded.into()
 }
