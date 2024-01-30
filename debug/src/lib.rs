6441a8db8520523881f933e0ef6a5d292f4718c7
Alex Fu <alex.yfu@gmail.com>
2024-01-30 19:19:54 +0800

    CustomDebug-04 solution optimized

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, parse_quote, DeriveInput, WherePredicate};
 
 mod utils;
 use utils::*;
@@ -25,24 +25,40 @@ pub fn derive(input: TokenStream) -> TokenStream {
     // If derived struct has generic type.
     let struct_generic = &ast.generics;
     // println!("{:#?}", struct_generic);
-    let is_generic_struct = struct_generic.params.len() > 0;
-    // Key: Restriction: T must implement Debug
-    let (impl_generics, ty_generics, raw_where_clause) = ast.generics.split_for_impl();
-    let where_clause = if is_generic_struct {
-        quote! {
-            where
-            #raw_where_clause
-            T: std::fmt::Debug + std::fmt::Display
-        }
+
+    // Key 1. Get generic variables
+    let (impl_generics, ty_generics, where_clause) = struct_generic.split_for_impl();
+
+    // Key 2. Process, there will be four situations
+    //  a. Derive struct does not have generic type
+    //  b. Derive struct has generic types but it doesn't have where clause
+    //  c. Derive struct has generic types and it has where clause
+    //  d. Derive struct has generic types and it has Debug & Display in where clause (ignore, leave it to compiler.)
+
+    let is_generic_struct = ast.generics.params.len() > 0;
+    let debug_where_clause = if is_generic_struct {
+        let debug_predicate: WherePredicate = parse_quote!(T: std::fmt::Debug + std::fmt::Display);
+        let new_where_clause = if where_clause.is_none() {
+            // b
+            quote! {
+                where #debug_predicate
+            }
+        } else {
+            // c
+            quote! {
+                #where_clause
+                #debug_predicate
+            }
+        };
+        new_where_clause
     } else {
-        quote! {
-            where
-            #raw_where_clause
-        }
+        // a
+        quote! {}
     };
 
+    // Key 3. Populate code block correctly.
     let expanded = quote! {
-        impl #impl_generics std::fmt::Debug for #derive_struct_ident #ty_generics #where_clause {
+        impl #impl_generics std::fmt::Debug for #derive_struct_ident #ty_generics #debug_where_clause {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
                 f.debug_struct("Field")
                     #(#field_debug_inner)*
@@ -50,5 +66,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         }
     };
+
     expanded.into()
 }
