c22fea3c856d8133d571ef65195b152603fae94b
Alex Fu <alex.yfu@gmail.com>
2024-01-30 16:36:29 +0800

    CustomDebug-04 solution

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -5,27 +5,44 @@ use syn::{parse_macro_input, DeriveInput};
 mod utils;
 use utils::*;
 
-// Key 1. "debug" attribute configured here.
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
 
     let derive_struct_ident = ast.ident;
+    let struct_data = &ast.data;
 
     // initialize helpers
-    let helpers = match init_debug_macro_helpers(&ast.data) {
+    let helpers = match init_debug_macro_helpers(struct_data) {
         Ok(vec) => vec,
         Err(e) => {
             return e;
         }
     };
 
-    // Key 1.1 generate fmt fn, the inner part: .field("field_name", field_value);
     let field_debug_inner = helpers.iter().map(|f| f.to_debug_inner_form());
 
-    // Key 1.2 complete the fmt fn.
+    // If derived struct has generic type.
+    let struct_generic = &ast.generics;
+    // println!("{:#?}", struct_generic);
+    let is_generic_struct = struct_generic.params.len() > 0;
+    // Key: Restriction: T must implement Debug
+    let (impl_generics, ty_generics, raw_where_clause) = ast.generics.split_for_impl();
+    let where_clause = if is_generic_struct {
+        quote! {
+            where
+            #raw_where_clause
+            T: std::fmt::Debug + std::fmt::Display
+        }
+    } else {
+        quote! {
+            where
+            #raw_where_clause
+        }
+    };
+
     let expanded = quote! {
-        impl std::fmt::Debug for #derive_struct_ident {
+        impl #impl_generics std::fmt::Debug for #derive_struct_ident #ty_generics #where_clause {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
                 f.debug_struct("Field")
                     #(#field_debug_inner)*
--- main.rs
+++ main.rs
@@ -1,9 +1,40 @@
-// Write code here.
+// Figure out what impl needs to be generated for the Debug impl of Field<T>.
+// This will involve adding a trait bound to the T type parameter of the
+// generated impl.
 //
-// To see what the code looks like after macro expansion:
-//     $ cargo expand
+// Callers should be free to instantiate Field<T> with a type parameter T which
+// does not implement Debug, but such a Field<T> will not fulfill the trait
+// bounds of the generated Debug impl and so will not be printable via Debug.
 //
-// To run the code:
-//     $ cargo run
+//
+// Resources:
+//
+//   - Representation of generics in the Syn syntax tree:
+//     https://docs.rs/syn/2.0/syn/struct.Generics.html
+//
+//   - A helper for placing generics into an impl signature:
+//     https://docs.rs/syn/2.0/syn/struct.Generics.html#method.split_for_impl
+//
+//   - Example code from Syn which deals with type parameters:
+//     https://github.com/dtolnay/syn/tree/master/examples/heapsize
 
-fn main() {}
+use derive_debug::CustomDebug;
+
+#[derive(CustomDebug)]
+pub struct Field<T> {
+    value: T,
+    #[debug = "0b{:08b}"]
+    bitmask: u8,
+}
+
+fn main() {
+    let f = Field {
+        value: "F",
+        bitmask: 0b00011100,
+    };
+
+    let debug = format!("{:?}", f);
+    let expected = r#"Field { value: "F", bitmask: 0b00011100 }"#;
+
+    assert_eq!(debug, expected);
+}
